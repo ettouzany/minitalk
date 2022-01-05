321a9b6d85eb6c654e13918cac7b43c459ad0683
Noureddine Et-touzany <net-touz@e2r9p6.1337.ma>
2022-01-05 23:26:31 +0100

    doner

--- client.c
+++ client.c
@@ -4,54 +4,37 @@
 #include <signal.h>
 #include <string.h>
 int pid;
-void from_char_to_bit(int c)
-{
-    printf("%d: \n", c);
-    int i = 0;
-    int j = 0;
-    char str[8];
-
-    while (c)
-    {
-        str[i] = c % 2;
-        i++;
-        c /= 2;
-    }
-    // printf("%s: \n", str);
-    while (i + j < 8 && 0 > c)
-    {
-        kill(pid, SIGUSR1);
-        j++;
-        usleep(10);
-    }
-    while (i + j < 8 && 0 < c)
-    {
-        kill(pid, SIGUSR2);
-        j++;
-        usleep(10);
-    }
-    while (i--)
-    {
-        if (!str[i])
-            kill(pid, SIGUSR1);
-        else
-            kill(pid, SIGUSR2);
-        usleep(10);
-    }
-    // printf("\n");
-}
-
 void shit_tfker(char c)
 {
-    int i = 0;
-    while (i++ < 8)
+    int i = 8;
+    while (i--)
     {
-        kill(pid, SIGUSR1 + (c & 1));
-        c = c >> 1;
-        usleep(10000);
+        kill(pid, SIGUSR1 + ((c >> i) & 1));
+        
+        usleep(200);
     }
 }
 
+void handler_sigusr(int sig, siginfo_t *info, void *context)
+{
+    if(sig == 31)
+    {
+        write(1, "good", 4);
+    }
+    else{
+        write(1, "not good", 8);
+    }
+}
+
+static void HandleHostSignal(void)
+{
+    struct sigaction satmp;
+    satmp.sa_flags = SA_SIGINFO;
+    satmp.sa_sigaction = handler_sigusr;
+
+    sigaction(SIGUSR1, &satmp, NULL);
+    sigaction(SIGUSR2, &satmp, NULL);
+}
 int main(int argc, char *argv[])
 {
     int i = 0;
@@ -61,6 +44,7 @@ int main(int argc, char *argv[])
         printf("client: invalid arguments\n");
         exit(EXIT_FAILURE);
     }
+     HandleHostSignal();
     pid = atoi(argv[1]);
     while (i < strlen(argv[2]))
     {
@@ -68,7 +52,15 @@ int main(int argc, char *argv[])
         shit_tfker(argv[2][i]);
         i++;
     }
-    usleep(10000);
+    usleep(200);
     shit_tfker(0);
+    //signal(SIGUSR2, handler_sigusr);
+        usleep(200);
+
+    //pause();
+    //signal(SIGUSR2, handler_sigusr);
+   
+//    while (1)
+//         usleep(1);
     return (0);
 }
--- server.c
+++ server.c
@@ -1,19 +1,3 @@
-// #include <unistd.h>
-// #include <stdio.h>
-// #include <signal.h>
-// int val = 10;
-
-// void handler(int sig)
-// {
-//     val += 5;
-// }
-
-// int main(int argc, char const *argv[])
-// {
-//     printf("%d", (int)getpid());
-//     signal(SIGCHLD, handler);
-//     return 0;
-// }
 
 #include <unistd.h>
 #include <signal.h>
@@ -24,81 +8,7 @@
 #include <stdint.h>
 #include <libc.h>
 
-// void catcher(int signum)
-// {
-//     puts("inside catcher...");
-// }
-
-// void timestamp()
-// {
-//     time_t t;
-//     time(&t);
-//     printf("the time is %s", ctime(&t));
-// }
-
-// main()
-// {
-//     printf("%d\n", (int)getpid());
-//     struct sigaction sigact;
-
-//     // sigemptyset(&sigact.sa_mask);
-//     // sigact.sa_flags = 0;
-//     //  sigact.sa_handler = catcher;
-//     sigaction(SIGUSR1, &sigact, NULL);
-
-//     // alarm(10);
-//     printf("before pause... ");
-//     // timestamp();
-//     pause();
-//     if (1)
-//         pause();
-//     printf("after pause... ");
-//     timestamp();
-//     return (0);
-// }
-
-// server.c
-// int count = 0;
-// int j = 0;
-// int convert(long long n)
-// {
-//     int dec = 0, i = 0, rem;
-//     while (n != 0)
-//     {
-//         rem = n % 10;
-//         n /= 10;
-//         dec += rem * pow(2, i);
-//         ++i;
-//     }
-//     str[i] = dec;
-//     return dec;
-// }
 char *str = NULL;
-
-char reverseBits(char n)
-{
-
-    char rev = 0;
-    // traversing bits of 'n' from the right
-    int i = 0;
-    while (i++ < 8)
-    {
-        // bitwise left shift
-        // 'rev' by 1
-        rev <<= 1;
-
-        // if current bit is '1'
-        if ((n & 1))
-            rev ^= 1;
-
-        // bitwise right shift
-        // 'n' by 1
-        n >>= 1;
-    }
-
-    // required number
-    return rev;
-}
 
 char *add_one()
 {
@@ -106,7 +16,7 @@ char *add_one()
     if (!str)
         return NULL;
 
-    s = malloc(sizeof(char) * strlen(str) + 1);
+    s = malloc(sizeof(char) * strlen(str) + 2);
     if (!s)
         return ((write(1, "notaloc\n", 8)), NULL);
     int i = 0;
@@ -115,33 +25,33 @@ char *add_one()
         s[i] = str[i];
         i++;
     }
+    s[i++] = 0;
     s[i] = 0;
     return (free(str), s);
 }
 
-void handler_sigusr(int signum)
+void    handler_sigusr(int sig, siginfo_t *info, void *context)
 {
     static int count;
     static int j;
-    // char w = 48 + signum - 30;
-    // write(1, &w, 1);
     if (count == 0 && j == 0)
     {
-        str = malloc(sizeof(char));
+        str = malloc(sizeof(char)+1);
         if (!str)
             (write(1, "notaloc\n", 8));
         str[count] = 0;
+        str[count+1] = 0;
     }
     if (str)
-        str[count] = !(signum) ? str[count] << 1 : (str[count] << 1) + 1;
+        str[count] = !(sig-30) ? str[count] << 1 : (str[count] << 1) + 1;
     j++;
     if (j > 7)
     {
-        // w = count + '0';
         if (str[count] == 0)
         {
             write(1, str, count);
-            // write(1, "\n", 1);
+            if(info->si_pid)
+                kill(info->si_pid, SIGUSR2);
             free(str);
             str = NULL;
             count = 0;
@@ -149,284 +59,33 @@ void handler_sigusr(int signum)
         }
         else
         {
-            str[count] = reverseBits(str[count]);
             count++;
             str = add_one();
         }
         j = 0;
-
-        // str[0] >> 5 != 6 || str[0] >> 4 != 14 || str[0] >> 3 != 60
-        // if (!(str[0] >> 7))
-        // {
-        //     write(1, &str, 1);
-        //     count = 0;
-        //     str[count] = 0;
-        // }
-        // else if (str[0] >> 5 == 6 && str[1] >> 6 == 2)
-        // {
-        //     write(1, &str, 2);
-        //     str[count] = 0;
-        //     while (count)
-        //     {
-        //         str[--count] = 0;
-        //     }
-        // }
-        // else if (str[0] >> 4 == 14 && str[1] >> 6 == 2 && str[2] >> 6 == 2)
-        // {
-        //     write(1, &str, 3);
-        //     str[count] = 0;
-        //     while (count)
-        //     {
-        //         str[--count] = 0;
-        //     }
-        // }
-        // else if ((str[0] >> 4 == 60 && str[1] >> 6 == 2 && str[2] >> 6 == 2 && str[3] >> 6 == 2) || count == 3)
-        // {
-        //     write(1, &str, 4);
-        //     str[count] = 0;
-        //     while (count)
-        //     {
-        //         str[--count] = 0;
-        //     }
-        // }
-        // else
-        // {
-        //     count++;
-        // }
     }
 }
-// void handler_sigusr2(int signum)
-// {
-//     str[count] = '0';
-//     i++;
-//     // write(1, "|", 1);
-//     if (i == 8)
-//     {
-//         convert(atoi(str));
-//         write(1, &c, 1);
-//         i = 0;
-//     }
-// }
+static void HandleHostSignal(void)
+{
+    struct sigaction satmp;
+    satmp.sa_flags = SA_SIGINFO;
+    satmp.sa_sigaction = handler_sigusr;
 
+    sigaction(SIGUSR1, &satmp, NULL);
+    sigaction(SIGUSR2, &satmp, NULL);
+}
 int main(void)
 {
-    // pid_t pid;
-    //  printf("%d", reverseBits(50));
-    // struct sigaction new_action;
-    // new_action.sa_handler = handler_sigusr;
-    // pid = getpid();
-    // printf("%d\n", pid);
-    // sigaction(SIGUSR1, &new_action, NULL);
-    // sigaction(SIGUSR2, &new_action, NULL);
-    // while (1)
-    // usleep(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
-    handler_sigusr(1);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(0);
-    handler_sigusr(1);
+    pid_t pid;
+    //struct sigaction new_action;
+       // new_action.sa_flags = 0;
+    //new_action.sa_handler = handler_sigusr;
+    pid = getpid();
+    printf("%d\n", pid);
+    HandleHostSignal();
+    // sigaction(SIGUSR1, &new_action,NULL);
+    // sigaction(SIGUSR2, &new_action,NULL);
+    while (1)
+        usleep(1);
+    
 }
